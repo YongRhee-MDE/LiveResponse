71101557cca293d807b345d7e70f3647cc6618b3
Yong Rhee [MSFT] <56358587+YongRhee-MSFT@users.noreply.github.com>
2020-06-10 21:24:24 -0700

    Update GetACompleteMemoryDump.ps1

--- GetACompleteMemoryDump.ps1
+++ GetACompleteMemoryDump.ps1
@@ -1,2 +1,2 @@
 Antonio Vasconcelos Live Response Github - Get a complete (kernel mode+user mode) memory dump
-https://github.com/anthonws/MDATP_PoSh_Scripts/tree/master/LiveResponse
+"https://github.com/anthonws/MDATP_PoSh_Scripts/tree/master/LiveResponse"
